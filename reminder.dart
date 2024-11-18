732a247ed81ac453374f6ffa2f47d753170d1ce9
sirapatkan <summonspaw@gmail.com>
2024-11-18 15:37:30 +0700

    firstdart

---
+++ reminder.dart
@@ -0,0 +1,9 @@
+void main(){
+  int num1=10;
+  int num2=3;
+
+  int totalquotient = num1~/num2;
+  int remainder = num1%num2;
+  print("totalquotient = $totalquotient");
+  print("remainder = $remainder");
+}
